3bffe6a1095ca5e9c966f83ea7ab5f0f907820c4
basit-dev <bas677@icloud.com>
2021-03-19 14:50:01 +0000

    Fibonacci sequence completed, added index.html

--- fibo.js
+++ fibo.js
@@ -1,25 +1,18 @@
-// Get fibo numbers
+// 0+1=1
+// 1+2=3
+// 3+2=5
+// 5+3=8
 
-function getFibo(num) {
-    let input = 0;
-    let output = 1;
- for (let i = 0; i < num; i++) {
-    // 0
-    // 1
-    // 2
+function getFib(num) {
+  let a = 0;
+  let b = 1;
+  let c = a + b;
 
-    input = output++; 
-    // 0 - input(0) + output(1) = 1
-    // 1 - input(1) + output(1) = 2
-    // 2 - input(2) + output(1) = 3 
-    // 3 - input(3) + output(2) = 5
-    // 4 - input(5) + output(3) = 8
-    // 5 - input(8) + output(5) = 13
-    // 6 - input(13) + output(8) = 21
-    // 7 - input(21) + output(13) = 34
-     console.log(input);
-     
- }
+  for (let i = num; c < num; i++) {
+    b = a;
+    a = c;
+    c = a + b;
+    console.log(c);
+  }
 }
-
-getFibo(5)
+getFib(10000);
