27f86318a1681bca319fa8147c83d13641cd4ea6
basit-dev <bas677@icloud.com>
2021-03-20 18:50:20 +0000

    get fib even numbers from a given input

--- fibo.js
+++ fibo.js
@@ -15,11 +15,33 @@ function getFib(num) {
     console.log(c);
   }
 }
-// getFib(10);
+getFib(20)
 
-// Recursive function
+
+// Recursive function to get the fifth fib position
   function fibRecursion(num) {
     if (num <= 1) return 1;
     return fibRecursion(num -1) + fibRecursion(num -2);
   }
 console.log(fibRecursion(5)) 
+
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+function getFibEvenNumbers(num) {
+  let a = 0;
+  let b = 1;
+  let c = a + b;
+  let getEven = 0;
+
+  for (let i = num; c < num; i++) {
+    b = a;
+    a = c;
+    c = a + b;
+    //console.log(c);
+    if (c % 2 == 0 ) getEven += c;
+  
+  }
+  return getEven;
+}
+console.log(getFibEvenNumbers(4000000))
+
