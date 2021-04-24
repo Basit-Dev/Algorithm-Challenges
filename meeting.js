581117395452cf905421a6f9dc8ebab008ec0229
basit-dev <bas677@icloud.com>
2021-04-24 03:07:19 +0100

    Refactored with map

--- meeting.js
+++ meeting.js
@@ -46,5 +46,11 @@ console.log(
     "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
   )
 );
-
-  
+// Refactored with map
+function meeting(s) {
+    let string = s.toUpperCase().split(';')
+                  .map(x => x.split(':').reverse().join(', '))
+                  .sort()
+                  .join(')(')
+    return '(' + string + ')'
+  }
