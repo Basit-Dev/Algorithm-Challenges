cff676d0a9ed3305d61a095e857ce57f005a4382
basit-dev <bas677@icloud.com>
2021-04-24 03:09:20 +0100

    Function name changed

--- meeting.js
+++ meeting.js
@@ -47,10 +47,15 @@ console.log(
   )
 );
 // Refactored with map
-function meeting(s) {
+function meetingTwo(s) {
     let string = s.toUpperCase().split(';')
                   .map(x => x.split(':').reverse().join(', '))
                   .sort()
                   .join(')(')
     return '(' + string + ')'
   }
+  console.log(
+    meetingTwo(
+      "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
+    )
+  );
