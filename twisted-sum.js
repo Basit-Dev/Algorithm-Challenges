e0b7d847dc8a4b6923bc3608457fc9f9d69d7208
basit-dev <bas677@icloud.com>
2021-04-25 03:07:49 +0100

    Completed

---
+++ twisted-sum.js
@@ -0,0 +1,22 @@
+/*Find the sum of the digits of all the numbers from 1 to N (both ends included).*/
+
+function twistedSum(n) {
+  let numToString = [];
+  let stringResult = 0;
+  let number = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i <= 9) {
+      console.log(i);
+      number += i;
+    } else if (i >= 9) {
+        numToString = i.toString().split("");
+      for (let i = 0; i < numToString.length; i++) {
+        stringResult += parseInt(numToString[i]) 
+      }
+    }
+  }
+  console.log(stringResult);
+  return number + stringResult;
+}
+
+console.log(twistedSum(999));
