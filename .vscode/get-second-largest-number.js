f991dd0ee232830a7276bb40f39c62b128909181
basit-dev <bas677@icloud.com>
2021-05-22 15:06:36 +0100

    completed

---
+++ .vscode/get-second-largest-number.js
@@ -0,0 +1,6 @@
+// function getSecondLargest(nums) {
+//     return Math.max(...nums) -1
+// }
+const getSecondLargest = (nums) => Math.max(...nums) -1;
+
+console.log(getSecondLargest([1,2,3,6,6,5]))
