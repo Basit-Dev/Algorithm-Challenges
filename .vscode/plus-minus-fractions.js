b15c70146bef3bfb875dba5f047fc32f228c1fa1
basit-dev <bas677@icloud.com>
2021-05-23 16:12:07 +0100

    refactored

--- .vscode/plus-minus-fractions.js
+++ .vscode/plus-minus-fractions.js
@@ -1,4 +1,5 @@
-/*Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.*/
+/*Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+There are 3 positive numbers, 2 negative numbers, and 1 zero in the array, this is the divided by the arr length.*/
 
 function plusMinus(arr) {
   // Get how many positive nunbers
@@ -10,17 +11,13 @@ function plusMinus(arr) {
   // Loop through and filter
   arr.filter((el) => {
     if (el > 0) {
-      positiveNum += 1;
+      positiveNum += 1 /arr.length;
     } else if (el < 0) {
-      negativeNum += 1;
+      negativeNum += 1 /arr.length;
     } else {
-      zeroNum += 1;
+      zeroNum += 1 /arr.length;
     }
   });
-  positiveNum /= arr.length;
-  negativeNum /= arr.length;
-  zeroNum /= arr.length;
-
   return positiveNum.toFixed(6) + " " + negativeNum.toFixed(6)+ " " + zeroNum.toFixed(6);
 }
 console.log(plusMinus([-4, 3, -9, 0, 4, 1]));
